285f971fccf3deed65ec4a7326e704cc55b198ed
Lucas Marins <lucasmarins2024@gmail.com>
2026-05-03 23:30:52 -0300

    fix(#10): correção de bugs

--- controle-horas/src/main/java/com/api/controle_horas/ControleHorasApplication.java
+++ controle-horas/src/main/java/com/api/controle_horas/ControleHorasApplication.java
@@ -2,12 +2,13 @@ package com.api.controle_horas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ControleHorasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControleHorasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ControleHorasApplication.class, args);
+    }
 }
--- controle-horas/src/main/java/com/api/controle_horas/modelos/dto/RegistrosHorasDto.java
+++ controle-horas/src/main/java/com/api/controle_horas/modelos/dto/RegistrosHorasDto.java
@@ -1,13 +1,13 @@
 package com.api.controle_horas.modelos.dto;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class RegistrosHorasDto {
-  
-  private List<ControleHorasDto> registros;
+public class RegistrosHorasDto implements Serializable {
 
+  private List<ControleHorasDto> registros;
   private Long tempoMinutos;
 }
